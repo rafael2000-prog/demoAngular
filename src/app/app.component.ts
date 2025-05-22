613d0f81d82bb71d12b6308399aec1f28c38ddd2
Rafael Serrano <rafsantiserrano@hotmail.com>
2025-05-22 02:53:26 -0500

    Introducir datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HolaUpsComponent } from './componet/hola-ups.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HolaUpsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "angulardemo111", appId: "1:267263325122:web:f68f26c903f7b74447dc5b", storageBucket: "angulardemo111.firebasestorage.app", apiKey: "AIzaSyDtNiq6gHk-DWS4OoqCTVO4FVU9HWkSYvA", authDomain: "angulardemo111.firebaseapp.com", messagingSenderId: "267263325122" })), provideFirestore(() => getFirestore())]
 };
